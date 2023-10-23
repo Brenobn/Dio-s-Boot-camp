b456f3ed5f437a272acbf1a96bb7a9540437e302
Brenobn <brenobragabn@gmail.com>
2023-10-23 10:40:34 -0300

    Create a class hero

---
+++ src/index.js
@@ -0,0 +1,13 @@
+class hero {
+  constructor(name, age, typeOfHero) {
+    this.name = name;
+    this.age = age;
+    this.typeOfHero = typeOfHero;
+  }
+
+  attack() {
+    
+  }
+}
+
+let mago = new hero("Orion", 102, "Mago");
